d81dfc5d27d2ff5394e22ec129385256f34dc8b4
rezaulorangetoolz <rezaul.orangetoolz@gmail.com>
2025-08-21 12:31:33 +0600

    rename file folder

--- tauri-app/src/main.tsx
+++
@@ -1,5 +0,0 @@
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router";
-import { router } from "./core/routes/routes";
-
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<RouterProvider router={router}/>);
