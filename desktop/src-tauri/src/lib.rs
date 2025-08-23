37cfa16fdb20683a23be43b84588af03e625628a
rezaulorangetoolz <rezaul.orangetoolz@gmail.com>
2025-08-23 23:22:20 +0600

    add

--- desktop/src-tauri/src/lib.rs
+++ desktop/src-tauri/src/lib.rs
@@ -1,16 +1,16 @@
-use xcap::Monitor;
-use tauri::{AppHandle, Emitter};
-use rdev::{listen,Event, EventType};
 use base64::{engine::general_purpose, Engine as _};
 use image::ImageFormat;
+use rdev::{listen, Event, EventType};
 use std::io::Cursor;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::OnceLock;
+use tauri::{AppHandle, Emitter};
+use xcap::Monitor;
 
 static IS_ALREADY_CALL: OnceLock<AtomicBool> = OnceLock::new();
 
 #[tauri::command]
-async fn get_screenshot () -> Result<Vec<String>, String>{
+async fn get_screenshot() -> Result<Vec<String>, String> {
     // Get all monitors
     let monitors = Monitor::all().map_err(|e| e.to_string())?;
     if monitors.is_empty() {
@@ -25,7 +25,9 @@ async fn get_screenshot () -> Result<Vec<String>, String>{
 
         // Convert image to PNG bytes in memory
         let mut buf = Cursor::new(Vec::new());
-        image.write_to(&mut buf, ImageFormat::Png).map_err(|e| e.to_string())?;
+        image
+            .write_to(&mut buf, ImageFormat::Png)
+            .map_err(|e| e.to_string())?;
 
         // Encode to base64
         let encoded = general_purpose::STANDARD.encode(buf.get_ref());
@@ -35,10 +37,8 @@ async fn get_screenshot () -> Result<Vec<String>, String>{
     Ok(base64_images)
 }
 
-
 #[tauri::command]
-fn monitor_activity(app:AppHandle) -> Result<String, String> {
-
+fn monitor_activity(app: AppHandle) -> Result<String, String> {
     // Initialize the flag if it's not yet initialized
     let flag = IS_ALREADY_CALL.get_or_init(|| AtomicBool::new(false));
 
@@ -49,26 +49,16 @@ fn monitor_activity(app:AppHandle) -> Result<String, String> {
 
     let app_handle = app.clone();
     let callback = move |event: Event| {
-            let event_payload = match event.event_type {
-            EventType::KeyPress(key) => {
-                Some(format!("Keyboard: {:?}", key))
-            },
-            EventType::KeyRelease(key) => {
-                Some(format!("Keyboard: {:?} released", key))
-            },
-            EventType::ButtonPress(button) => {
-                Some(format!("Mouse: {:?} pressed", button))
-            },
-            EventType::ButtonRelease(button) => {
-                Some(format!("Mouse: {:?} released", button))
-            },
-            EventType::MouseMove { x, y } => {
-                Some(format!("Mouse: moved to ({}, {})", x, y))
-            },
+        let event_payload = match event.event_type {
+            EventType::KeyPress(key) => Some(format!("Keyboard: {:?}", key)),
+            EventType::KeyRelease(key) => Some(format!("Keyboard: {:?} released", key)),
+            EventType::ButtonPress(button) => Some(format!("Mouse: {:?} pressed", button)),
+            EventType::ButtonRelease(button) => Some(format!("Mouse: {:?} released", button)),
+            EventType::MouseMove { x, y } => Some(format!("Mouse: moved to ({}, {})", x, y)),
             _ => None, // Ignore other event types
-            };
+        };
 
-            // If we have a valid payload, emit it as an event to the frontend.
+        // If we have a valid payload, emit it as an event to the frontend.
         if let Some(payload) = event_payload {
             app_handle.emit("MY_EVENT", payload).unwrap();
         }
@@ -78,17 +68,15 @@ fn monitor_activity(app:AppHandle) -> Result<String, String> {
         println!("Error listening for events: {:?}", error);
     };
 
-
     Ok("call done".to_string())
-
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![get_screenshot,monitor_activity])
+        .invoke_handler(tauri::generate_handler![get_screenshot, monitor_activity])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
-
--- desktop/src/features/setup/Setup.tsx
+++ desktop/src/features/setup/Setup.tsx
@@ -5,16 +5,23 @@ import { useNavigate } from "react-router";
 import type { SubmitHandler } from "react-hook-form";
 import { useForm } from "react-hook-form"
 import type { ISetupFormTypes } from "@/types";
+import { useApp } from "@/core/hooks/useApp";
 
 
 
 export default function Setup() {
   const navigate = useNavigate();
-  const {handleSubmit, register} = useForm<ISetupFormTypes>()
+  const {handleSubmit, register, formState:{errors}} = useForm<ISetupFormTypes>()
+  const {setStore}= useApp()
 
 
-  const onSubmit:SubmitHandler<ISetupFormTypes> = (data) => {
-    navigate("/login", {state:{...data}})
+  const onSubmit:SubmitHandler<ISetupFormTypes> = async (data) => {
+    try {
+      await setStore("BASE_URL", data.baseUrl)
+      navigate("/login")
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
@@ -24,7 +31,14 @@ export default function Setup() {
           defaultValue=""
           placeholder="Past base url"
           id="url"
-          register={register('baseUrl', {required: "Base url is required"})}
+          register={register('baseUrl', {
+            required: "Base url is required",
+            pattern: {
+              value: /^(https?:\/\/)([a-zA-Z0-9.-]+)(:[0-9]+)?(\/.*)?$/,
+              message: "Enter a valid URL (must start with http:// or https://)",
+            },
+          })}
+          error={errors.baseUrl?.message}
         />
         <Button text="Next" size="md" type="submit"/>
       </form>
